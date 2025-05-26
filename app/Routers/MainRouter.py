829e540427fecd0cebb504de4ba5b97ece2a2db6
CCF2P <yura.smolyaninov.01@mail.ru>
2025-05-26 18:14:01 +0800

    added support for JSON responses for frontend frameworks

--- app/Routers/MainRouter.py
+++ app/Routers/MainRouter.py
@@ -9,7 +9,7 @@ from fastapi import (
     Depends,
     HTTPException,
     File,
-    UploadFile
+    UploadFile,
 )
 
 # Build path to "Templates" folder from OS root folder
@@ -27,68 +27,70 @@ main_router = APIRouter()
 async def get_index_html(request: Request):
     try:
         return templates.TemplateResponse(
-            name='index.html',
+            name="index.html",
             context={'request': request}
         )
     except:
-        return templates.TemplateResponse(
-            name='error.html',
-            context={
-                'request': request,
-                "error_message": "Opps! Some error occurred."
-            }
-        )
+        if "text/html" in request.headers.get("Accept"):
+            return templates.TemplateResponse(
+                name="error.html",
+                context={
+                    "request": request,
+                    "error_message": "Page is not found."
+                },
+                status_code=500
+            )
+        elif "application/json" in request.headers.get("Accept"):
+            return HTTPException(
+                detail="Page is not found.",
+                status_code=500
+            )
 
 
 @main_router.get(path="/upload", summary="Upload page")
 async def get_index_html(request: Request):
     try:
         return templates.TemplateResponse(
-            name='upload.html',
-            context={'request': request}
+            name="upload.html",
+            context={"request": request}
         )
     except:
-        return templates.TemplateResponse(
-            name='error.html',
-            context={
-                'request': request,
-                "error_message": "Opps! Some error occurred."
-            }
-        )
+        if "text/html" in request.headers.get("Accept"):
+            return templates.TemplateResponse(
+                name="error.html",
+                context={
+                    "request": request,
+                    "error_message": "Page is not found."
+                },
+                status_code=500
+            )
+        elif "application/json" in request.headers.get("Accept"):
+            return HTTPException(
+                detail="Page is not found.",
+                status_code=500
+            )
 
 
 @main_router.get(path="/compare", summary="Compare page")
 async def get_index_html(request: Request):
     try:
+        print(request.headers.get("Accept"))
         return templates.TemplateResponse(
-            name='compare.html',
-            context={'request': request}
+            name="compare.html",
+            context={"request": request}
         )
     except:
-        return templates.TemplateResponse(
-            name='error.html',
-            context={
-                'request': request,
-                "error_message": "Opps! Some error occurred."
-            }
-        )
-
-
-'''@main_router.post(path="/upload/")
-def upload_files(
-    user_images: List[UploadFile]=File(...),
-    db: Session=Depends(get_db)
-):
-    for user_image in user_images:
-        if not user_image.filename.endswith('.dcm'):
-            raise HTTPException(status_code=400, detail="Only DICOM files are allowed")
-        
-        existing_feature = crud.get_feature_by_identifier(db, user_image.filename)
-        if existing_feature:
-            continue
-
-        features = compare.extract_features(user_image, vgg19_model)
-        features = json.dumps(features.tolist())
-        db_feature = crud.create_feature(db, features, identifier=user_image.filename)
-    
-    return RedirectResponse(url="/", status_code=303)'''
+        if "text/html" in request.headers.get("Accept"):
+            return templates.TemplateResponse(
+                name="error.html",
+                context={
+                    "request": request,
+                    "error_message": "Page is not found."
+                },
+                status_code=500
+            )
+        elif "application/json" in request.headers.get("Accept"):
+            return HTTPException(
+                detail="Page is not found.",
+                status_code=500
+            )
